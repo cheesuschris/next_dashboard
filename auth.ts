f8abb56e97c0c04f2c5e172c698a28a7776212b3
Chris Wu <qiangwu6@gmail.com>
2025-09-19 20:38:11 -0400

    fixed await

--- auth.ts
+++ auth.ts
@@ -8,9 +8,11 @@ import postgres from "postgres";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
-async function getUser(email: String): Promise<User | undefined> {
+async function getUser(email: string): Promise<User | undefined> {
   try {
-    const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
+    const user = await (sql<
+      User[]
+    >`SELECT * FROM users WHERE email=${email}` as unknown as Promise<User[]>);
     return user[0];
   } catch (error) {
     console.error("Failed to fetch user:", error);
